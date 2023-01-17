95558ac409248abe7ded38647d8d1c4ea1ef6740
devoremcintosh <devoremcintosh@gmail.com>
2023-01-17 12:59:37 -0500

    first commit

---
+++ 3Parameters.js
@@ -0,0 +1,5 @@
+function stats(name, height, country) {
+    console.log('My name is ' + name + ' and my height is ' + height + ' and I am from the ' + country);
+}
+
+stats('Devore', 'Sevetny two Inches Tall', 'United States');
